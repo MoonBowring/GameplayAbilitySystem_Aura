b6fde193f57d4ee383d24fa54f4326f2b2c3500e
L <1953562425@qq.com>
2026-09-13 15:43:14 +0800

    Broadcasting Effect Asset Tags

--- Source/TopDownViewDEMO/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/TopDownViewDEMO/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -16,9 +16,5 @@ void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* Ability
 {
 	FGameplayTagContainer TagContainer;
 	EffectSpec.GetAllAssetTags(TagContainer);
-	for (const FGameplayTag& Tag : TagContainer)//把 TagContainer 里的每一个 GameplayTag 一个一个拿出来
-	{
-		const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
-		GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Blue, Msg);
-	}
+	EffectAssetTags.Broadcast(TagContainer);
 }
--- Source/TopDownViewDEMO/Public/AbilitySystem/AuraAbilitySystemComponent.h
+++ Source/TopDownViewDEMO/Public/AbilitySystem/AuraAbilitySystemComponent.h
@@ -6,6 +6,8 @@
 #include "AbilitySystemComponent.h"
 #include "AuraAbilitySystemComponent.generated.h"
 
+DECLARE_MULTICAST_DELEGATE_OneParam(FEffectAssentTags, const FGameplayTagContainer& /* 资产标签 */);
+
 /**
  * 
  */
@@ -17,6 +19,8 @@ class TOPDOWNVIEWDEMO_API UAuraAbilitySystemComponent : public UAbilitySystemCom
 public:
 	//ActorInfo 设置完成以后要做的事情
 	void AbilityActorInfoSet();
+	
+	FEffectAssentTags EffectAssetTags;
 	
 protected:
 	void EffectApplied(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle);
