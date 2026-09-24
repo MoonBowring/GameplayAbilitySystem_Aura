42cbac61fc76a32a34da558329254198fb43ba40
L <1953562425@qq.com>
2026-09-24 13:15:25 +0800

    Native Gameplay Tags

--- Source/TopDownViewDEMO/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/TopDownViewDEMO/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -3,12 +3,18 @@
 
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
 
+#include "AuraGameplayTags.h"
+
 void UAuraAbilitySystemComponent::AbilityActorInfoSet()
 {
 	//以后这个 ASC 收到 Gameplay Effect 的时候，调用我自己的 EffectApplied()
 	//也就是告诉这个 Delegate 以后发生 OnGameplayEffectAppliedDelegateToSelf 的时候, 请通知我 并且调用我的 EffectApplied 函数
 	//GameplayEffect 被应用到这个 ASC 时，这个 Delegate 被触发
 	OnGameplayEffectAppliedDelegateToSelf.AddUObject(this, &UAuraAbilitySystemComponent::EffectApplied);
+	
+	const FAuraGameplayTags& GameplayTags = FAuraGameplayTags::Get();
+	//GameplayTags.Attributes_Secondary_Armor.ToString();
+	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Orange, FString::Printf(TEXT("Tag: %s"), *GameplayTags.Attributes_Secondary_Armor.ToString()));
 }
 
 void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent,
