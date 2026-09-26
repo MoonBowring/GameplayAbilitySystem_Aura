779f02fcd227512341d9768424d3a44f0a5b6e62
L <1953562425@qq.com>
2026-09-26 12:44:33 +0800

    Aura Ability System Blueprint Library

---
+++ Source/TopDownViewDEMO/Public/AbilitySystem/AuraAbilitySystemLibrary.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "AuraAbilitySystemLibrary.generated.h"
+
+class UOverlayWidgetController;
+
+UCLASS()
+class TOPDOWNVIEWDEMO_API UAuraAbilitySystemLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+	UFUNCTION(BlueprintPure, Category = "AuraAbilitySystemLiberary|WidgetController")
+	static UOverlayWidgetController* GetOverlayWidgetController(const UObject* WorldContextObject);
+};
